6a72c3b055991315e60200b1384eabbbd444e71f
helixplant <143057647+helixplant@users.noreply.github.com>
2023-09-26 23:07:39 -0400

    javascript error fix attempt

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 let noOfCharac = 150;
-let contents = document.querySelectorAll(".middle-section");
-/*
-contents.forEach(content => {
-        if(content.textContent.length < noOfCharac){
-            content.nextElementSibling.style.display = "none";
-        } else {
-            let displayText = content.textContent.slice(0, noOfCharac);
-            let moreText = content.textContent.slice(noOfCharac);
-            content.innerHTML = '${displayText}<span class="dots">...</span><span class="hide more">${moreText}</span>'
-
-    }
-});*/
+let contents = document.querySelectorAll("#middle-section");
 
 function readMore(btn){
     let post = btn.parentElement;
