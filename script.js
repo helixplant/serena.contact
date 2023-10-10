a03d4883b491fca2b2ec38a856dcd7af36d5e5fa
helixplant <143057647+helixplant@users.noreply.github.com>
2023-10-10 04:03:18 -0400

    New Website Version

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-function readMore(btn) {
-    let post = btn.parentElement;
-    let dots = post.querySelector(".dots");
-    let moreText = post.querySelector(".more");
-    
-    dots.classList.toggle("hide");
-    moreText.classList.toggle("hide");
-  
-    btn.textContent = moreText.classList.contains("hide") ? "Read More" : "Read Less";
-  }
-  
+
+
+
+
+	function readMore(btn) {
+		let post = btn.parentElement;
+		let dots = post.querySelector(".dots");
+		let moreText = post.querySelector(".more");
+		
+		dots.classList.toggle("hide");
+		moreText.classList.toggle("hide");
+	  
+		btn.textContent = moreText.classList.contains("hide") ? "Read More" : "Read Less";
+	  }
+	  
